2b365c4850fad55a5ac0a91f72c4c4f08af4b0d8
Jyrki Jakonen <jyrki.jakonen@gmail.com>
2017-07-12 18:03:33 +0300

    reading by line from file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Test
+// Reads file line by line
+
+let fs = require('fs')
+let readline = require('readline')
+let inputFile = fs.createReadStream('links.txt')
+
+const rl = readline.createInterface({
+   input: inputFile,
+   output: process.stdout
+});
+
+rl.on('line', (input) => {
+   let split = input.split(' ');
+   console.log( input ) 
+});
