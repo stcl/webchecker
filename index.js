668a84956ad3619a169b5aea23d6fc8034ea6208
Jyrki Jakonen <jyrki.jakonen@gmail.com>
2017-07-16 12:45:03 +0300

    logging + config

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-// Reads file line by line
+/* 
+    Reads file line by line
+*/
 
+// Other files
+const config = require('./config.json')
+
+// 1st party packages
+const fs = require('fs');
+
+// 3rd party packages
+const winston = require('winston')
 const express = require('express')
 const app = express()
-
 const url = require('url')
 const request = require('request')
-
 const bodyParser = require('body-parser')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+//app.get('/', function (req, res) {
+//  res.send('Hello World!')
+//})
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      colorize: true,
+      level: 'info'
+    }),
+    new (winston.transports.File)({
+      filename: `results.log`,
+      //timestamp: tsFormat,
+      level: 'info',
+      prepend: true
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    })
+  ]
+});
 
-app.post('/', function (req, res) {
+//app.listen(3000, function () {
+//  console.log('Example app listening on port 3000!')
+//})
+
+/*app.post('/', function (req, res) {
     var parsed_url = url.format({
         query: {
         q: req.body.text
         }
     });
     res.send('Web page status: ' + parsed_url );
-});
+});*/
 
-let fs = require('fs')
 let readline = require('readline')
 let inputFile = fs.createReadStream('links.txt')
 var lines;
@@ -51,27 +75,33 @@ function goThrough( input ) {
         url: element,
         timeout: 10000
     }
+    
+    var start = new Date();     // timer set
+
     request.get(opts, function (err, res, body) {
-        
+
+        var responseTime = new Date() - start;
+        console.log( responseTime );
         // Get the content type
         let contenttype = res.headers['content-type'];
         // Compare that the line exists in content-type
         if (contenttype.includes(info))
             console.log("Type matches")
-
         if (err)
             console.log(err);
-        if (!err && res.statusCode === 200) {
+        if (res.statusCode === 200) {
             console.log("Page OK");
             console.log(res.statusCode);
         }
-    });
 
-    //}, this);
+        // logging
+        logger.info( input + " " + res.statusCode + " " + responseTime )
+
+    });
 }
 
 rl.on('line', (input) => {    
     // make timed interval
-    setInterval( goThrough, 1500, input);
+    setInterval( goThrough, config.waittime, input);
 });
 
