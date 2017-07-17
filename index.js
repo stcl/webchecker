6fadc558d0ae5547ca2d5efa5c36d41ce34f7202
Jyrki Jakonen <jyrki.jakonen@gmail.com>
2017-07-17 15:24:04 +0300

    moved request function to monitor.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Other files
 const config = require('./config.json')
 const logger = require('./logger.js')
+const monitor = require('./monitor.js')
 
 // 1st party packages
 const fs = require('fs');
@@ -12,13 +13,9 @@ const fs = require('fs');
 // 3rd party packages
 const winston = require('winston')
 const express = require('express')
-const app = express()
+//const app = express()
 const url = require('url')
 const request = require('request')
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 var myArgs = process.argv.slice(2);
 if( myArgs[0] ) {
@@ -27,51 +24,22 @@ if( myArgs[0] ) {
 
 let readline = require('readline')
 let inputFile = fs.createReadStream('links.txt')
-var lines;
 
 const rl = readline.createInterface({
     input: inputFile,
     output: process.stdout
 });
 
-
 // Takes array of addresses to go though all
-function makeRequest( input ) {
 
-    winston.debug( "input: " + input )
-
-    let element = input.split(' ', 1).toString()
-    let info = input.split(' ', 2)[1];
-
-    var opts = {
-        url: element,
-        timeout: 10000
-    }
-    
-    var start = new Date();     // timer set
-
-    request.get(opts, function (err, res, body) {
-
-        var responseTime = new Date() - start;
-        // Get the content type
-        let contenttype = res.headers['content-type']
-        // Compare that the line exists in content-type
-        if (!contenttype.includes(info))
-            winston.info("Type does not match: " + info )
-        // Get error case
-        if (err)
-            winston.error(err);
-        else if (res.statusCode === 200) {
-            winston.info("Page OK");
-        }
-        // logging
-        logger.info( "URL: " + element + " Statuscode: " + res.statusCode + " Responsetime: " + responseTime )
-
-    });
-}
-
-rl.on('line', (input) => {    
+rl.on('line', (input) => {
     // make timed interval
-    setInterval( makeRequest, config.waittime, input);
+    setInterval( monitor.makeRequest, config.waittime, input, function(res) {
+        logger.info( "URL: " + res.element + 
+                    " Statuscode: " + res.statusCode + 
+                    " Responsetime: " + res.responseTime )
+        }
+    )
+
 });
 
---
+++ monitor.js
@@ -0,0 +1,45 @@
+/*
+    Monitoring the request according to input
+*/
+const config = require('./config.json')
+const winston = require('winston')
+const request = require('request')
+
+module.exports.makeRequest = function( input, callback ) {
+
+    winston.debug( "input: " + input )
+
+    // todo check that input is alright
+
+    let element = input.split(' ', 1).toString()
+    let info = input.split(' ', 2)[1]
+
+    const opts = {
+        url: element,
+        timeout: config.timeout | 10000
+    }
+
+    const start = new Date()    // timer set
+
+    request.get(opts, function (err, res, body) {
+
+        var responseTime = new Date() - start;
+        // Get the content type
+        let contenttype = res.headers['content-type']
+        // Compare that the line exists in content-type
+        if (!contenttype.includes(info))
+            winston.info("Type does not match: " + info )
+        // Get error case
+        if (err)
+            winston.error(err);
+        else if (res.statusCode === 200) {
+            winston.info("Page OK");
+        }
+
+        res.element = element;
+        res.responseTime = responseTime;
+
+        callback( res );
+        // logging
+    });
+}
